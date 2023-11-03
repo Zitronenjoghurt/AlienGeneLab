0d146cf825b2544ac40cd78684ca799e2304a51b
Zitronenjoghurt <gersdorfdominik@gmail.com>
2023-11-03 18:22:59 +0100

    performance profiling with generating 100.000 aliens

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import pstats
 from src.classes.alien import Alien
 
 def main():
-    for _ in range(0, 1000):
-        alien1 = Alien.generate_random()
-        alien1.export_to_json('alien.json')
+    for _ in range(0, 100000):
+        alien = Alien.generate_random()
 
 if __name__ == "__main__":
     profiler = cProfile.Profile()

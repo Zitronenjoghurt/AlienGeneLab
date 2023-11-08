ad44d9e81dc51963052cb4dbe58c0b717556899a
Zitronenjoghurt <gersdorfdominik@gmail.com>
2023-11-08 18:48:00 +0100

    optimize base4 calculations and add option to flip characters

--- src/modules/genetic_code.py
+++ src/modules/genetic_code.py
@@ -1,48 +1,51 @@
-CODE = {0: 'A', 1: 'G', 2: 'C', 3: 'T'}
-REVERSED_CODE = {value: key for key, value in CODE.items()}
+import numpy as np
 
+# Configuration
 CODE_LENGTH = 4
+CODE = {0: 'A', 1: 'G', 2: 'C', 3: 'T'}
+
+# Precalculation
+REVERSED_CODE = {value: key for key, value in CODE.items()}
+INDICES = np.array(list(CODE.keys()))
+VALUES = set(CODE.values())
 
 def encode(values: list) -> str:
-    result = ""
-
-    for value in values:
-        result += to_base4(value)
-    
-    return result
+    result = [to_base4(value) for value in values]
+    return ''.join(result)
 
 def decode(code: str) -> list:
-    # Uppercase and filter code
     code = code.upper()
-    code = ''.join([char for char in code if char in CODE.values()])
+    code = ''.join(filter(VALUES.__contains__, code))
 
-    # Split code into equally sized blocks
-    values = [code[i:i+CODE_LENGTH] for i in range(0, len(code), CODE_LENGTH)]
-
-    result = []
-    for value in values:
-        result.append(from_base4(value))
+    block_count = len(code) // CODE_LENGTH
+    result = [None] * block_count
+    
+    for i in range(block_count):
+        block = code[i*CODE_LENGTH:(i+1)*CODE_LENGTH]
+        result[i] = from_base4(block)
     
     return result
 
 def to_base4(value: int) -> str:
-    if value == 0:
-        return CODE[0]*4
+    result = [CODE[0]] * CODE_LENGTH
     
-    result = []
-    while value > 0:
+    index = CODE_LENGTH - 1
+    
+    while value > 0 and index >= 0:
         remainder = value % 4
-        result.append(CODE[remainder])
-        value = value // 4
+        result[index] = CODE[remainder]
+        value //= 4
+        index -= 1
 
-    base4 = ''.join(reversed(result))
-    return base4.rjust(CODE_LENGTH, CODE[0])
+    return ''.join(result)
 
 def from_base4(string: str) -> int:
     value = 0
+    power = 1
 
-    for i, char in enumerate(reversed(string)):
-        value += REVERSED_CODE[char] * (4 ** i)
+    for char in string[::-1]:
+        value += REVERSED_CODE[char] * power
+        power *= CODE_LENGTH
     
     return value
 
@@ -62,3 +65,12 @@ def count_char(string: str, char: str) -> int:
             result += 1
 
     return result
+
+# chance between 0 and 1
+def flip_code(code: str, chance: float) -> str:
+    result = [flip_char(char) if np.random.rand() < chance else char for char in code]
+    return ''.join(result)
+
+def flip_char(char: str) -> str:
+    indices = INDICES[INDICES != REVERSED_CODE[char]]
+    return CODE[np.random.choice(indices)]
--- tests/test_genetic_code.py
+++ tests/test_genetic_code.py
@@ -1,4 +1,4 @@
-from src.modules.genetic_code import encode, decode, to_base4, from_base4
+from src.modules.genetic_code import encode, decode, to_base4, from_base4, flip_char
 
 def test_encode():
     assert encode([0, 1, 2, 3, 255]) == 'AAAAAAAGAAACAAATTTTT'
@@ -6,3 +6,40 @@ def test_encode():
 def test_decode():
     assert decode('AAAAAAAGAAACAAATTTTT') == [0, 1, 2, 3, 255]
     assert decode('aAAAxxvbbnAAAg31.;sdAAACAAATTTTT') == [0, 1, 2, 3, 255]
+
+def test_to_base4():
+    assert to_base4(0) == 'AAAA'
+    assert to_base4(1) == 'AAAG'
+    assert to_base4(2) == 'AAAC'
+    assert to_base4(3) == 'AAAT'
+    assert to_base4(4) == 'AAGA'
+    assert to_base4(5) == 'AAGG'
+    assert to_base4(6) == 'AAGC'
+    assert to_base4(7) == 'AAGT'
+    assert to_base4(8) == 'AACA'
+    assert to_base4(9) == 'AACG'
+    assert to_base4(10) == 'AACC'
+    assert to_base4(11) == 'AACT'
+    assert to_base4(12) == 'AATA'
+    assert to_base4(13) == 'AATG'
+    assert to_base4(14) == 'AATC'
+    assert to_base4(15) == 'AATT'
+    assert to_base4(16) == 'AGAA'
+    assert to_base4(255) == 'TTTT'
+
+def test_flip_char():
+    for _ in range (10000):
+        a = flip_char('A')
+        g = flip_char('G')
+        t = flip_char('T')
+        c = flip_char('C')
+
+        assert a != 'A'
+        assert g != 'G'
+        assert t != 'T'
+        assert c != 'C'
+
+        assert a in ['G', 'T', 'C']
+        assert g in ['A', 'T', 'C']
+        assert t in ['A', 'G', 'C']
+        assert c in ['A', 'G', 'T']
